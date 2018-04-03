419aeab9b1b63c63d1d1679101a9e9609e2568db
Théo Winterhalter <isheeft@gmail.com>
2018-04-03 16:18:49 +0200

    Redefine eq_term as with eq_dec

---
+++ Translation/Equality.v
@@ -0,0 +1,158 @@
+From Coq Require Import Bool String List BinPos Compare_dec Omega.
+From Equations Require Import Equations DepElimDec.
+From Template Require Import Ast utils Typing.
+From Translation Require Import util SAst SInduction.
+
+(*! Equality between terms *)
+(* This goes through the definition of a nameless syntax *)
+
+Inductive nlterm : Type :=
+| nlRel (n : nat)
+| nlSort (s : sort)
+| nlProd (A B : nlterm)
+| nlLambda (A B t : nlterm)
+| nlApp (u : nlterm) (A B v : nlterm)
+(* Homogenous equality *)
+| nlEq (A u v : nlterm)
+| nlRefl (A u : nlterm)
+| nlJ (A u P w v p : nlterm)
+| nlTransport (T1 T2 p t : nlterm)
+(* Heterogenous equality *)
+| nlHeq (A a B b : nlterm)
+| nlHeqToEq (p : nlterm)
+| nlHeqRefl (A a : nlterm)
+| nlHeqSym (p : nlterm)
+| nlHeqTrans (p q : nlterm)
+| nlHeqTransport (p t : nlterm)
+| nlCongProd (B1 B2 pA pB : nlterm)
+| nlCongLambda (B1 B2 t1 t2 pA pB pt : nlterm)
+| nlCongApp (B1 B2 pu pA pB pv : nlterm)
+| nlCongEq (pA pu pv : nlterm)
+| nlCongRefl (pA pu : nlterm)
+| nlEqToHeq (p : nlterm)
+| nlHeqTypeEq (p : nlterm)
+(* Packing *)
+| nlPack (A1 A2 : nlterm)
+| nlProjT1 (p : nlterm)
+| nlProjT2 (p : nlterm)
+| nlProjTe (p : nlterm)
+(* Inductives *)
+| nlInd (ind : inductive)
+| nlConstruct (ind : inductive) (n : nat)
+| nlCase (indn : inductive * nat) (p c : nlterm) (brs : list (nat * nlterm))
+.
+
+Fixpoint nl (t : sterm) : nlterm :=
+  match t with
+  | sRel n => nlRel n
+  | sSort s => nlSort s
+  | sProd n A B => nlProd (nl A) (nl B)
+  | sLambda n A B t => nlLambda (nl A) (nl B) (nl t)
+  | sApp u n A B v => nlApp (nl u) (nl A) (nl B) (nl v)
+  | sEq A u v => nlEq (nl A) (nl u) (nl v)
+  | sRefl A u => nlRefl (nl A) (nl u)
+  | sJ A u P w v p => nlJ (nl A) (nl u) (nl P) (nl w) (nl v) (nl p)
+  | sTransport T1 T2 p t => nlTransport (nl T1) (nl T2) (nl p) (nl t)
+  | sHeq A a B b => nlHeq (nl A) (nl a) (nl B) (nl b)
+  | sHeqToEq p => nlHeqToEq (nl p)
+  | sHeqRefl A a => nlHeqRefl (nl A) (nl a)
+  | sHeqSym p => nlHeqSym (nl p)
+  | sHeqTrans p q => nlHeqTrans (nl p) (nl q)
+  | sHeqTransport p t => nlHeqTransport (nl p) (nl t)
+  | sCongProd B1 B2 pA pB => nlCongProd (nl B1) (nl B2) (nl pA) (nl pB)
+  | sCongLambda B1 B2 t1 t2 pA pB pt =>
+    nlCongLambda (nl B1) (nl B2) (nl t1) (nl t2) (nl pA) (nl pB) (nl pt)
+  | sCongApp B1 B2 pu pA pB pv =>
+    nlCongApp (nl B1) (nl B2) (nl pu) (nl pA) (nl pB) (nl pv)
+  | sCongEq pA pu pv => nlCongEq (nl pA) (nl pu) (nl pv)
+  | sCongRefl pA pu => nlCongRefl (nl pA) (nl pu)
+  | sEqToHeq p => nlEqToHeq (nl p)
+  | sHeqTypeEq p => nlHeqTypeEq (nl p)
+  | sPack A1 A2 => nlPack (nl A1) (nl A2)
+  | sProjT1 p => nlProjT1 (nl p)
+  | sProjT2 p => nlProjT2 (nl p)
+  | sProjTe p => nlProjTe (nl p)
+  | sInd ind => nlInd ind
+  | sConstruct ind n => nlConstruct ind n
+  | sCase indn p c brs => nlCase indn (nl p) (nl c) (map (on_snd nl) brs)
+  end.
+
+Fact inductive_dec : forall (i i' : inductive), { i = i'} + { i <> i' }.
+Proof.
+  intros i i'.
+  destruct i as [s i], i' as [s' i'].
+  case (string_dec s' s) ; intro e.
+  - subst. case (Nat.eq_dec i' i) ; intro e.
+    + subst. left. reflexivity.
+    + right. intro h. apply e. now inversion h.
+  - right. intro h. apply e. now inversion h.
+Defined.
+
+
+Section nldec.
+
+  Ltac finish :=
+    let h := fresh "h" in
+    right ;
+    match goal with
+    | e : ?t <> ?u |- _ =>
+      intro h ; apply e ; now inversion h
+    end.
+
+  Ltac fcase c :=
+    let e := fresh "e" in
+    case c ; intro e ; [subst ; try (left ; reflexivity) | finish].
+
+  Ltac nl_dec_tac nl_dec :=
+    repeat match goal with
+           | t : nlterm, u : nlterm |- _ => fcase (nl_dec t u)
+           | s : sort, z : sort |- _ => fcase (Nat.eq_dec s z)
+           | n : nat, m : nat |- _ => fcase (Nat.eq_dec n m)
+           | i : inductive, i' : inductive |- _ => fcase (inductive_dec i i')
+           end.
+
+  Fixpoint nl_dec (t u : nlterm) : { t = u } + { t <> u }.
+  Proof.
+    destruct t ; destruct u ; try (right ; discriminate).
+    all: try (nl_dec_tac nl_dec).
+    destruct indn as [i' n'], indn0 as [i n]. nl_dec_tac nl_dec.
+    assert (dec : forall (x y : nat * nlterm), { x = y } + { x <> y }).
+    { intros [? ?] [? ?]. nl_dec_tac nl_dec. }
+    fcase (list_eq_dec dec brs brs0).
+  Defined.
+
+End nldec.
+
+Definition eq_term (t u : sterm) : bool :=
+  if nl_dec (nl t) (nl u) then true else false.
+
+Fact eq_term_refl :
+  forall {t}, eq_term t t = true.
+Proof.
+  intro t. unfold eq_term.
+  case (nl_dec (nl t) (nl t)).
+  - intro. reflexivity.
+  - intro h. exfalso. apply h. reflexivity.
+Defined.
+
+Fact eq_term_sym :
+  forall {t u}, eq_term t u = true -> eq_term u t = true.
+Proof.
+  unfold eq_term.
+  intros t u.
+  case (nl_dec (nl u) (nl t)) ; intro e.
+  - reflexivity.
+  - case (nl_dec (nl t) (nl u)) ; intro e'.
+    + exfalso. apply e. easy.
+    + intro. easy.
+Defined.
+
+Fact eq_term_trans :
+  forall {t u v}, eq_term t u = true -> eq_term u v = true -> eq_term t v = true.
+Proof.
+  intros t u v.
+  unfold eq_term.
+  case (nl_dec (nl t) (nl u)) ; intro e1.
+  - intros _. rewrite e1. auto.
+  - discriminate.
+Defined.
