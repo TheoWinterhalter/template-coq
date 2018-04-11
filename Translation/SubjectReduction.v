8212d14b24ea7833d1e0187103f348cf8dfda9ef
Théo Winterhalter <isheeft@gmail.com>
2018-04-11 16:20:52 +0200

    Beginning of subject reduction

--- Translation/SubjectReduction.v
+++ Translation/SubjectReduction.v
@@ -150,14 +150,73 @@ Theorem subj_red :
 Proof.
   intros Σ Γ t u T hg hr ht. revert Γ T ht.
   induction hr.
-  - intros Γ T ht. dependent destruction ht.
-    + ttinv ht3. destruct (prod_inv h1).
-      eapply typing_subst ; try eassumption.
-      eapply type_conv ; try eassumption.
-      eapply type_ctxconv ; try eassumption.
-      econstructor ; try eassumption.
+  - intros Γ T ht.
+    destruct (istype_type hg ht).
+    ttinv ht. ttinv h3.
+    destruct (prod_inv h6).
+    eapply type_conv ; try eassumption.
+    eapply typing_subst ; try eassumption.
+    eapply type_conv ; try eassumption.
+    eapply type_ctxconv ; try eassumption.
+    + econstructor ; try eassumption.
+      eapply typing_wf. eassumption.
+    + constructor ; try assumption.
       apply ctxconv_refl.
-    + ttinv ht1. ttinv h3. destruct (prod_inv h6).
+  - intros Γ T ht.
+    destruct (istype_type hg ht).
+    ttinv ht. ttinv h3.
+    destruct (eq_conv_inv h8) as [[? ?] ?].
+    eapply type_conv ; try eassumption.
+    eapply conv_trans ; try eassumption.
+    apply cong_subst.
+    + apply conv_sym.
+      apply cong_Refl ; assumption.
+    + apply substs_conv. eapply conv_trans ; try eassumption.
+      apply conv_sym. assumption.
+  - intros Γ T' ht.
+    destruct (istype_type hg ht).
+    ttinv ht. ttinv h.
+    destruct (eq_conv_inv h6) as [[? ?] ?].
+    eapply type_conv ; try eassumption.
+    eapply conv_trans.
+    + eapply conv_sym. eassumption.
+    + eapply conv_trans ; [| eassumption ]. assumption.
+  - intros Γ T ht.
+    ttinv ht. destruct (istype_type hg ht).
+    specialize (IHhr _ _ ltac:(eassumption)).
+    pose proof (conv_red_l _ _ _ _ hr (conv_refl _ _)).
+    eapply type_conv ; try eassumption.
+    eapply type_conv.
+    + econstructor ; try eassumption.
+      * eapply type_ctxconv ; try eassumption.
+        -- econstructor ; try eassumption.
+           eapply typing_wf. eassumption.
+        -- constructor ; try assumption.
+           apply ctxconv_refl.
+      * eapply type_ctxconv ; try eassumption.
+        -- econstructor ; try eassumption.
+           eapply typing_wf. eassumption.
+        -- constructor ; try assumption.
+           apply ctxconv_refl.
+    + econstructor ; eassumption.
+    + eapply cong_Prod ;
+      try (apply conv_refl) ;
+      try assumption ;
+      try (apply conv_sym ; assumption).
+  - intros Γ T ht.
+    ttinv ht. destruct (istype_type hg ht).
+    specialize (IHhr _ _ ltac:(eassumption)).
+    pose proof (conv_red_l _ _ _ _ hr (conv_refl _ _)).
+    eapply type_conv ; try eassumption.
+    eapply type_conv.
+    + econstructor ; try eassumption.
+      econstructor ; eassumption.
+    + econstructor ; eassumption.
+    + (* conv rewrite H0. *)
+      eapply cong_Prod ;
+      try (apply conv_refl) ;
+      try assumption ;
+      try (apply conv_sym ; assumption).
 Admitted.
 
 Theorem subj_conv :
