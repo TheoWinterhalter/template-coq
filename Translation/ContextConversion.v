09e50d1a802c586e2b3b6a1ca2ec5621894f9848
Théo Winterhalter <isheeft@gmail.com>
2018-04-14 18:34:43 +0200

    New version of context conversion almost proved

--- Translation/ContextConversion.v
+++ Translation/ContextConversion.v
@@ -55,9 +55,152 @@ Ltac lift_sort :=
 
 Section ctxconv.
 
+  Fact safe_nth_conv :
+    forall {Σ Γ Δ},
+      ctxconv Σ Γ Δ ->
+      forall n is1 is2,
+        Σ |-i safe_nth Γ (exist _ n is1) = safe_nth Δ (exist _ n is2).
+  Proof.
+    intros Σ Γ Δ h. induction h ; intros n is1 is2.
+    - cbn. bang.
+    - destruct n.
+      + cbn. assumption.
+      + cbn. apply IHh.
+  Defined.
+
   (* This should be the normal definition. *)
   Definition isType Σ Γ A :=
     exists s, Σ ;;; Γ |-i A : sSort s.
+
+  Inductive ctxctx Σ : scontext -> scontext -> Prop :=
+  | ctxctx_nil : ctxctx Σ [] []
+  | ctxctx_cons Γ Δ A B :
+      ctxctx Σ Γ Δ ->
+      isType Σ Δ A ->
+      ctxctx Σ (Γ,, A) (Δ,, B).
+
+  Ltac tih type_ctxconv' :=
+    lazymatch goal with
+    | |- _ ;;; (?Δ,, ?A),, ?B |-i _ : _ =>
+      eapply type_ctxconv' ; [
+        eassumption
+      | assumption
+      | econstructor ; [
+          econstructor ; [ assumption | tih type_ctxconv' ]
+        | idtac
+        ]
+      | econstructor ; [
+          econstructor ; [ assumption | eexists ; tih type_ctxconv' ]
+        | idtac
+        ]
+      | econstructor ; [
+          econstructor ; [ assumption | apply conv_refl ]
+        | apply conv_refl
+        ]
+      ]
+    | |- _ ;;; ?Δ,, ?A |-i _ : _ =>
+      eapply type_ctxconv' ; [
+        eassumption
+      | assumption
+      | econstructor ; [ assumption | tih type_ctxconv' ]
+      | econstructor ; [ assumption | eexists ; tih type_ctxconv' ]
+      | econstructor ; [ assumption | apply conv_refl ]
+      ]
+    | |- _ ;;; _ |-i _ : _ =>
+      eapply type_ctxconv' ; eassumption
+    | _ => fail "Not applicable tih"
+    end.
+
+  Ltac ih :=
+    lazymatch goal with
+    | type_ctxconv' :
+        forall (Σ : sglobal_context) (Γ Δ : scontext) (t A : sterm),
+          Σ;;; Γ |-i t : A ->
+          type_glob Σ ->
+          wf Σ Δ ->
+          ctxctx Σ Γ Δ ->
+          ctxconv Σ Γ Δ ->
+          Σ;;; Δ |-i t : A
+      |- _ => tih type_ctxconv'
+    | _ => fail "Cannot find type_ctxconv'"
+    end.
+
+  Fixpoint type_ctxconv' {Σ Γ Δ t A} (ht : Σ ;;; Γ |-i t : A) {struct ht} :
+    type_glob Σ ->
+    wf Σ Δ ->
+    ctxctx Σ Γ Δ ->
+    ctxconv Σ Γ Δ ->
+    Σ ;;; Δ |-i t : A.
+  Proof.
+    intros hg hw hcc hc. destruct ht.
+    all: try (econstructor ; ih).
+    - eapply type_conv.
+      + econstructor. assumption.
+      + lift_sort. cheat.
+      + apply lift_conv. apply conv_sym. apply safe_nth_conv. assumption.
+    - econstructor. assumption.
+    - econstructor.
+      + lift_sort. eapply typing_lift01 ; try eassumption ; ih.
+      + eapply typing_lift01 ; try eassumption ; ih.
+      + refine (type_Rel _ _ _ _ _) ; [| cbn ; omega ].
+        econstructor ; try eassumption. ih.
+    - eexists. econstructor.
+      + lift_sort. eapply typing_lift01 ; try eassumption ; ih.
+      + eapply typing_lift01 ; try eassumption ; ih.
+      + refine (type_Rel _ _ _ _ _) ; [| cbn ; omega ].
+        econstructor ; try eassumption. ih.
+    - eapply type_HeqTrans with (B := B) ; ih.
+    - econstructor ; try ih.
+      eapply type_ctxconv'.
+      + eassumption.
+      + assumption.
+      + econstructor ; try assumption.
+        econstructor ; ih.
+      + econstructor ; try assumption.
+        eexists ; econstructor ; ih.
+      + econstructor ; try assumption.
+        apply conv_refl.
+    - econstructor ; try ih.
+      + eapply type_ctxconv'.
+        * eassumption.
+        * assumption.
+        * econstructor ; try assumption.
+          econstructor ; ih.
+        * econstructor ; try assumption.
+          eexists ; econstructor ; ih.
+        * econstructor ; try assumption.
+          apply conv_refl.
+      + eapply type_ctxconv'.
+        * eassumption.
+        * assumption.
+        * econstructor ; try assumption.
+          econstructor ; ih.
+        * econstructor ; try assumption.
+          eexists ; econstructor ; ih.
+        * econstructor ; try assumption.
+          apply conv_refl.
+    - econstructor ; try ih.
+      eapply type_ctxconv'.
+      + eassumption.
+      + assumption.
+      + econstructor ; try assumption.
+        econstructor ; ih.
+      + econstructor ; try assumption.
+        eexists ; econstructor ; ih.
+      + econstructor ; try assumption.
+        apply conv_refl.
+    - eapply type_ProjT2 with (A1 := A1) ; ih.
+    - econstructor.
+      + assumption.
+      + eassumption.
+    - econstructor. assumption.
+    - econstructor.
+      + ih.
+      + ih.
+      + assumption.
+
+    Unshelve. exact 0. rewrite <- ctxconv_length by eassumption. assumption.
+  Qed.
 
   Ltac type_type hA tih type_ctxconv :=
     first [
@@ -111,19 +254,6 @@ Section ctxconv.
       |- _ => tih hA type_ctxconv
     | _ => fail "Cannot find type_ctxconv"
     end.
-
-  Fact safe_nth_conv :
-    forall {Σ Γ Δ},
-      ctxconv Σ Γ Δ ->
-      forall n is1 is2,
-        Σ |-i safe_nth Γ (exist _ n is1) = safe_nth Δ (exist _ n is2).
-  Proof.
-    intros Σ Γ Δ h. induction h ; intros n is1 is2.
-    - cbn. bang.
-    - destruct n.
-      + cbn. assumption.
-      + cbn. apply IHh.
-  Qed.
 
   Fixpoint type_ctxconv {Σ Γ Δ t A} (ht : Σ ;;; Γ |-i t : A) {struct ht} :
     type_glob Σ ->
