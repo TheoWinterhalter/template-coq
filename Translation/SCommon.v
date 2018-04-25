f49665489151ed8b02a4bd9b3f78e29f90a02017
Théo Winterhalter <isheeft@gmail.com>
2018-04-25 11:38:28 +0200

    Specify nctx_of_Prods

--- Translation/SCommon.v
+++ Translation/SCommon.v
@@ -50,6 +50,26 @@ Fixpoint nctx_of_Prods_acc (t : sterm) (Γ : nctx) : nctx * sterm :=
 
 Definition nctx_of_Prods t := nctx_of_Prods_acc t [].
 
+Fact nctx_of_Prods_acc_spec :
+  forall {t Γ},
+    let '(Δ, u) := nctx_of_Prods_acc t Γ in
+    Prods Γ t = Prods Δ u.
+Proof.
+  intros t. induction t ; intros Γ.
+  all: try (cbn ; reflexivity).
+  cbn. rewrite <- IHt2. cbn. reflexivity.
+Defined.
+
+Fact nctx_of_Prods_spec :
+  forall {t},
+    let '(Γ, u) := nctx_of_Prods t in
+    t = Prods Γ u.
+Proof.
+  intros t.
+  apply @nctx_of_Prods_acc_spec with (Γ := []).
+Defined.
+
+
 (* Common lemmata *)
 
 Definition max_sort := max.
