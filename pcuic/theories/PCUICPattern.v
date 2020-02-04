ca73ef129a7bbd3d297160071a25c8b3e6f48c02
Théo Winterhalter <isheeft@gmail.com>
2020-02-04 14:02:55 +0100

    [WIP] Primitive definition of patterns
    This will break everything temporarilly. The previous definition was broken for higher order and this will be an attempt to fix that.

--- pcuic/theories/PCUICPattern.v
+++ pcuic/theories/PCUICPattern.v
@@ -19,48 +19,61 @@ From MetaCoq.PCUIC Require Import PCUICAst PCUICAstUtils PCUICInduction
 
    TODO Maybe constraint pattern_variable so the list is without duplicates.
 *)
-Inductive pattern (npat : nat) (nb : nat) : term -> Prop :=
-| pattern_variable :
-    forall n l,
-      nb <= n ->
-      n < npat + nb ->
-      Forall (fun m => m < nb) l ->
-      pattern npat nb (mkApps (tRel n) (map tRel l))
+Inductive pattern (npat : nat) (nb : nat) : Type :=
+| pattern_variable n (mask : list bool) :
+    nb <= n ->
+    n < npat + nb ->
+    #|mask| = nb ->
+    pattern npat nb
 
-| pattern_bound :
-    forall n,
-      n < nb ->
-      pattern npat nb (tRel n)
+| pattern_bound n (bound : n < nb)
 
-| pattern_lambda :
-    forall na A t,
-      pattern npat nb A ->
-      pattern npat (S nb) t ->
-      pattern npat nb (tLambda na A t)
+| pattern_lambda (A : pattern npat nb) (b : pattern npat (S nb))
 
-| pattern_construct :
-    forall ind n ui args,
-      Forall (pattern npat nb) args ->
-      pattern npat nb (mkApps (tConstruct ind n ui) args)
+| pattern_construct
+    (ind : inductive) (n : nat) (ui : universe_instance)
+    (args : list (pattern npat nb)).
 
-| pattern_symbol :
-    forall k n ui args,
-      Forall (pattern npat nb) args ->
-      pattern npat nb (mkApps (tSymb k n ui) args)
-.
+Inductive elim_pattern (npat : nat) : Type :=
+| epApp (p : pattern npat 0)
+| epCase
+    (indn : inductive × nat) (p : pattern npat 0)
+    (brs : list (nat × pattern npat 0))
+| epProj (p : projection).
 
-Inductive elim_pattern (npat : nat) : elimination -> Prop :=
-| pat_elim_App :
-    forall p,
-      pattern npat 0 p ->
-      elim_pattern npat (eApp p)
+Fixpoint mask_to_rels (mask : list bool) (i : nat) :=
+  match mask with
+  | true :: mask => tRel i :: mask_to_rels mask (S i)
+  | false :: mask => mask_to_rels mask (S i)
+  | [] => []
+  end.
 
-| pat_elim_Case :
-    forall indn p brs,
-      pattern npat 0 p ->
-      Forall (fun br => pattern npat 0 br.2) brs ->
-      elim_pattern npat (eCase indn p brs)
+(* Translating patterns to terms
 
-| pat_elim_Proj :
-    forall p,
-      elim_pattern npat (eProj p).
+  Maybe it'd be smarter to define instantiation...
+*)
+Fixpoint pattern_to_term {npat nb} (p : pattern npat nb) : term :=
+  match p with
+  | pattern_variable n mask h1 h2 h3 =>
+    mkApps (tRel n) (mask_to_rels mask 0)
+  | pattern_bound n h => tRel n
+  | pattern_lambda A b => tLambda nAnon (pattern_to_term A) (pattern_to_term b)
+  | pattern_construct ind n ui args =>
+    mkApps (tConstruct ind n ui) (map (pattern_to_term) args)
+  end.
+
+Fixpoint elim_pattern_to_elim {npat} (e : elim_pattern npat) : elimination :=
+  match e with
+  | epApp p => eApp (pattern_to_term p)
+  | epCase indn p brs =>
+    eCase indn (pattern_to_term p) (map (on_snd (pattern_to_term)) brs)
+  | epProj p => eProj p
+  end.
+
+Notation partial_subst := (list (option term)).
+
+(* In order to apply the rules we need to define higher order matching.
+   Contrarily to first-order matching, we can't use substitution for
+   instantiation alone.
+*)
+(* Fixpoint match_pattern {npat nb} (p : pattern npat nb) (t : term) : option partial_subst *)
