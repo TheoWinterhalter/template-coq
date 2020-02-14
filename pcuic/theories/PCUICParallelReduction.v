979c7118a43ba3915d594128b5aad83f50a7e7e3
Théo Winterhalter <isheeft@gmail.com>
2020-02-14 14:13:00 +0100

    Prove is_rewrite_rule_linear

--- pcuic/theories/PCUICParallelReduction.v
+++ pcuic/theories/PCUICParallelReduction.v
@@ -4182,6 +4182,57 @@ Section ParallelSubstitution.
     - cbn. constructor.
   Qed.
 
+  Lemma declared_symbol_linear :
+    forall Σ k decl n r,
+      wf Σ ->
+      declared_symbol Σ k decl ->
+      nth_error decl.(rules) n = Some r ->
+      linear #|r.(pat_context)| r.(elims).
+  Proof.
+    intros Σ k decl n r hΣ h e.
+    unfold declared_symbol in h.
+    eapply lookup_on_global_env in h. 2: eauto.
+    destruct h as [Σ' [wfΣ' decl']].
+    red in decl'. red in decl'.
+    destruct decl' as [hctx [hr [hpr hprr]]].
+    eapply All_nth_error in hr. 2: eassumption.
+    destruct hr as [T hl hr hh hlin he].
+    assumption.
+  Qed.
+
+  Lemma declared_symbol_par_linear :
+    forall Σ k decl n r,
+      wf Σ ->
+      declared_symbol Σ k decl ->
+      nth_error decl.(prules) n = Some r ->
+      linear #|r.(pat_context)| r.(elims).
+  Proof.
+    intros Σ k decl n r hΣ h e.
+    unfold declared_symbol in h.
+    eapply lookup_on_global_env in h. 2: eauto.
+    destruct h as [Σ' [wfΣ' decl']].
+    red in decl'. red in decl'.
+    destruct decl' as [hctx [hr [hpr hprr]]].
+    eapply All_nth_error in hpr. 2: eassumption.
+    destruct hpr as [T hl hpr hh hlin he].
+    assumption.
+  Qed.
+
+  Lemma is_rewrite_rule_linear :
+    forall Σ k decl r,
+      wf Σ ->
+      is_rewrite_rule Σ k decl r ->
+      linear #|r.(pat_context)| r.(elims).
+  Proof.
+    intros Σ k decl r hΣ hr.
+    destruct hr as [h [[n e]|[n e]]].
+    - eapply declared_symbol_linear. all: eassumption.
+    - eapply declared_symbol_par_linear. all: eassumption.
+  Qed.
+
+  (* Lemma linear_mask_subst :
+    forall npat  *)
+
   (** When you do not apply a rewrite rule to a lhs only the substitution
     reduces.
   *)
@@ -4198,15 +4249,16 @@ Section ParallelSubstitution.
         el' = map (subst_elim σ' 0) el.
   Proof.
     intros Σ k ui decl r Γ Δ σ el' hΣ hr ss hσ el h.
+    eapply is_rewrite_rule_linear in hr as hlin. 2: auto.
     eapply elims_reduct in h. all: eauto.
     2:{
       apply is_rewrite_rule_pattern in hr. 2: auto.
       apply All_map. eapply All_impl. 1: eauto.
       intros e he. unfold compose.
-      (* We need to prove this, which is annoying but shouldn't
-        be a problem.
-      *)
-      admit.
+      eapply elim_pattern_subst in he. 1: eauto.
+      apply untyped_subslet_length in hσ.
+      rewrite subst_context_length in hσ.
+      lia.
     }
     (* We should do something similar with linearity
       presumably before the eapply.
